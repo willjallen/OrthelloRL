60d8201b3b53cf4198b7e439524d3a2127033a51
willjallen <wallen2268@gmail.com>
2022-10-22 18:45:06 -0500

    Cmake for little c boi

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
+# import tkinter as tk
+
+# class App(tk.Frame):
+#     def __init__(self, master=None):
+#         super().__init__(master)
+#         self.pack()
+
+# # create the application
+# myapp = App()
+
+# #
+# # here are method calls to the window manager class
+# #
+# myapp.master.title("My Do-Nothing Application")
+# myapp.master.maxsize(1000, 400)
+
+# # start the program
+# myapp.mainloop()
+
+
+
 from cgi import test
 from ctypes import *
 
 
-lib = WinDLL("C:\\Users\\WilliamAllen\\Desktop\\School\\thesis\\Orthello\\orthelloDLL.dll") # Not fine
+lib = WinDLL("C:\\Users\\WilliamAllen\\Desktop\\School\\thesis\\Othello\\othello.dll") # Not fine
 print(lib)
 
 testBoard = ((c_int * 8) * 8)()
@@ -22,7 +43,7 @@ for i in testBoard:
     print('')
     
 
-player = c_int(1)
+player = c_int(2)
 
 # https://stackoverflow.com/questions/58610333/c-function-called-from-python-via-ctypes-returns-incorrect-value/58611011#58611011
 lib.calculateLegalMoves.argtypes = (((c_int * 8) * 8), c_int)
@@ -34,3 +55,6 @@ for i in testBoard:
     for j in i:
         print(j, end='')
     print('')
+    
+    
+    
