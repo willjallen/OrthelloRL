1131462dd2b3185da65cad916453db93548326c3
willjallen <wallen2268@gmail.com>
2022-10-22 21:10:39 -0500

    Basic grid

--- main.py
+++ main.py
@@ -1,60 +1,191 @@
-# import tkinter as tk
-
-# class App(tk.Frame):
-#     def __init__(self, master=None):
-#         super().__init__(master)
-#         self.pack()
-
-# # create the application
-# myapp = App()
-
-# #
-# # here are method calls to the window manager class
-# #
-# myapp.master.title("My Do-Nothing Application")
-# myapp.master.maxsize(1000, 400)
-
-# # start the program
-# myapp.mainloop()
-
-
-
+from tkinter import *
+from tkinter import ttk
 from cgi import test
 from ctypes import *
 
 
-lib = WinDLL("C:\\Users\\WilliamAllen\\Desktop\\School\\thesis\\Othello\\othello.dll") # Not fine
-print(lib)
+# lib = WinDLL("C:\\Users\\WilliamAllen\\Desktop\\School\\thesis\\Othello\\othello.dll") # Not fine
+# print(lib)
 
-testBoard = ((c_int * 8) * 8)()
+# testBoard = ((c_int * 8) * 8)()
 
-# print(testBoard[9][0])
+# # print(testBoard[9][0])
 
-testBoard[3][3] = 1;
-testBoard[4][3] = 2;
-testBoard[3][4] = 2;
-testBoard[4][4] = 1;
+# testBoard[3][3] = 1;
+# testBoard[4][3] = 2;
+# testBoard[3][4] = 2;
+# testBoard[4][4] = 1;
 
-print('array ptr: ', byref(testBoard))
+# print('array ptr: ', byref(testBoard))
 
-for i in testBoard:
-    for j in i:
-        print(j, end='')
-    print('')
+# for i in testBoard:
+#     for j in i:
+#         print(j, end='')
+#     print('')
     
 
-player = c_int(2)
+# player = c_int(1)
 
-# https://stackoverflow.com/questions/58610333/c-function-called-from-python-via-ctypes-returns-incorrect-value/58611011#58611011
-lib.calculateLegalMoves.argtypes = (((c_int * 8) * 8), c_int)
-lib.calculateLegalMoves(testBoard, player);
+# # https://stackoverflow.com/questions/58610333/c-function-called-from-python-via-ctypes-returns-incorrect-value/58611011#58611011
+# lib.calculateLegalMoves.argtypes = (((c_int * 8) * 8), c_int)
+# lib.calculateLegalMoves(testBoard, player);
 
-print()
+# print()
 
-for i in testBoard:
-    for j in i:
-        print(j, end='')
-    print('')
+# for i in testBoard:
+#     for j in i:
+#         print(j, end='')
+#     print('')
     
+
+
+class Othello():
+    def __init__(self):
+        # Create the game state
+        self._board = ((c_int * 8) * 8)()
+        
+        self._current_player = c_int(1)
+        
+    def getTile(self, x, y):
+        return self._board[x][y]
+
+
+
+# a subclass of Canvas for dealing with resizing of windows
+class ResizingCanvas(Canvas):
+    def __init__(self,parent,**kwargs):
+        Canvas.__init__(self,parent,**kwargs)
+        self.bind("<Configure>", self.on_resize)
+        self.height = self.winfo_reqheight()
+        self.width = self.winfo_reqwidth()
+
+    def on_resize(self,event):
+        # determine the ratio of old width/height to new width/height
+        wscale = float(event.width)/self.width
+        hscale = float(event.height)/self.height
+        self.width = event.width
+        self.height = event.height
+        # resize the canvas 
+        
+        # This \/ causes the window the expand upon launch, maybe an issue to not include idk 
+        # self.config(width=self.width, height=self.height)
+        
+        # rescale all the objects tagged with the "all" tag
+        self.scale("all",0,0,wscale,hscale)
+
+
+class GUI():
+    def __init__(self, root, othello):
+        
+        self._othello = othello
+        
+        root.title("Othello")
+        
+        frame = Frame(root)
+        # frame.pack(fill=BOTH, expand=YES)
+        frame.grid(column=0, row=0, sticky=(N, W, E, S))
+        root.columnconfigure(0, weight=1)
+        root.rowconfigure(0, weight=1)
+        
+        
+        
+        
+        gameCanvas = ResizingCanvas(frame, width=400, height=400, bg="green")
+        gameCanvas.grid(column=0, row=0, sticky=(N,E,S,W))
+        frame.rowconfigure(0, weight=1)
+        frame.columnconfigure(0, weight=1)
+
+        ttk.Label(frame, text="feet").grid(column=1, row=0, sticky=N)
+        frame.rowconfigure(0, weight=1)
+        frame.columnconfigure(1, weight=1)
+        
+
+
+        # gameCanvas.pack(fill=BOTH, expand=YES)
+
+        
+        # gameCanvas = Canvas(root, width=400,height=400)
+
+        # gameCanvas.grid(row=0, column=0, sticky=N+S+E+W)
+        # gameCanvas.pack(fill="both", expand=True)
+
+        # gameCanvas.create_line(0, 0, 200, 100)
+        self.drawBoard(gameCanvas)
+
+        
+    
+    # Done once 
+    def drawBoard(self, gameCanvas):
+            
+        canvasWidth = gameCanvas.width
+        canvasHeight = gameCanvas.height
+        
+        cellLinesWidth = 2
+        borderLinesWidth = 4
+        
+        
+        # 10 lines total, 8 for grid and 2 for top and bottom
+        widthInterval = canvasWidth / 8
+        heightInterval = canvasHeight / 8
+        
+        # Draw 8x8 grid
+        
+
+        drawY = 1
+        
+        for row in range(0, 8):
+            gameCanvas.create_line(0, drawY, canvasWidth, drawY, width=cellLinesWidth)
+            drawY += heightInterval
+            
+        
+        drawX = 1 
+        
+        for col in range(0, 8):
+            gameCanvas.create_line(drawX, 0, drawX, canvasHeight, width=cellLinesWidth)
+            drawX += widthInterval
+            
+        gameCanvas.addtag_all("all")
+
+                
+    
+    def drawTiles(self):
+        pass
+
+
+       
+        # self.feet = StringVar()
+        # feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
+        # feet_entry.grid(column=2, row=1, sticky=(W, E))
+        # self.meters = StringVar()
+
+        # ttk.Label(mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(W, E))
+        # ttk.Button(mainframe, text="Calculate", command=self.calculate).grid(column=3, row=3, sticky=W)
+
+        # ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=W)
+        # ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=E)
+        # ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=W)
+
+        # for child in mainframe.winfo_children(): 
+        #     child.grid_configure(padx=5, pady=5)
+
+        # feet_entry.focus()
+        # root.bind("<Return>", self.calculate)
+
+    def calculate(self, *args):
+        try:
+            value = float(self.feet.get())
+            self.meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
+        except ValueError:
+            pass
+
+
+
+
+root = Tk()
+GUI(root, Othello())
+root.mainloop()
+
+
+
     
     
